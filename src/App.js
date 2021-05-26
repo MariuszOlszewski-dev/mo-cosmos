1fba7296355afe312a20e6d69a7c4c7649695eab
Mariusz Olszewski <ssssspam11@gmail.com>
2021-05-26 19:40:17 +0200

    Adding Prettier

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import React, {useState} from 'react';
-import './App.css';
-import Tiles from './Components/Tiles';
-import Modal from './Components/Modal';
-
+import React, { useState } from "react";
+import "./App.css";
+import Tiles from "./Components/Tiles";
+import Modal from "./Components/Modal";
 
 function App() {
-
   const [modalVisible, setModalVisible] = useState(false);
   const [anim, setAnim] = useState(0);
   const [anim2, setAnim2] = useState(0);
 
   return (
     <div className="App">
-      <div className="title"> <span className="titleVector1"></span> <span className="titleVector2"></span></div>
+      <div className="title">
+        {" "}
+        <span className="titleVector1"></span>{" "}
+        <span className="titleVector2"></span>
+      </div>
       <div className="leftPage">
         <Tiles setModalVisible={setModalVisible} />
-        {modalVisible !== false && <Modal modalVisible={modalVisible} setModalVisible={setModalVisible}/>}      
+        {modalVisible !== false && (
+          <Modal
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+          />
+        )}
       </div>
       <div className="rightPage">
-        <div 
-        className={anim2 ? 'cloudLeft anim2' : 'cloudLeft'}
-        onAnimationEnd={() => setAnim2(0)}
+        <div
+          className={anim2 ? "cloudLeft anim2" : "cloudLeft"}
+          onAnimationEnd={() => setAnim2(0)}
         ></div>
-        <div 
-          className={anim ? 'rocket anim' : 'rocket'}
+        <div
+          className={anim ? "rocket anim" : "rocket"}
           onAnimationEnd={() => setAnim(0)}
-        >
-        </div>
+        ></div>
         <div className="landing"></div>
-        <div 
-          className={anim2 ? 'cloudRight anim2' : 'cloudRight'}
+        <div
+          className={anim2 ? "cloudRight anim2" : "cloudRight"}
           onAnimationEnd={() => setAnim2(0)}
         ></div>
         <div className="planet"></div>
-        <button 
+        <button
           className="startRocket"
-          onClick={() => {setAnim(1); setAnim2(1)}}
+          onClick={() => {
+            setAnim(1);
+            setAnim2(1);
+          }}
         >
-        Lunch rocket</button>
+          Lunch rocket
+        </button>
       </div>
-      <div className="footer"><span className="leftFooter">Copyright © Usertive & SpaceX API</span><span className="rightFooter">Recruiment Task / Summer 2021</span></div>
+      <div className="footer">
+        <span className="leftFooter">Copyright © Usertive & SpaceX API</span>
+        <span className="rightFooter">Recruiment Task / Summer 2021</span>
+      </div>
     </div>
-
   );
 }
 
--- src/Components/Crew.jsx
+++ src/Components/Crew.jsx
@@ -1,74 +1,93 @@
-import React, {useState, useEffect} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown, faArrowUp, faSpinner} from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowDown,
+  faArrowUp,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Crew = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [crewList, setCrewList] = useState([]);
+  const [sortOrder, setSortOrder] = useState(0);
 
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [crewList, setCrewList] = useState([]);
-    const [sortOrder, setSortOrder] = useState(0);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://api.spacexdata.com/v4/crew`)
+      .then((response) => response.json())
+      .then((response) => {
+        setCrewList(
+          response.map((el) => {
+            return { name: el.name, status: el.status };
+          })
+        );
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError("Failed to API connection");
+      });
+  }, []);
 
-    useEffect(() => {
-        setLoading(true);
-        fetch(`https://api.spacexdata.com/v4/crew`)
-        .then(response => response.json())
-        .then(response => {
-            setCrewList(response.map(el => {
-                return {name: el.name, status: el.status}
-            }))
-            setLoading(false);
-        })
-        .catch(error => {
-            setLoading(false);
-            setError('Failed to API connection');
-        })
-    }, []);
-
-    const sortData = () =>{
-        if(sortOrder === 0 || sortOrder === 1){
-            crewList.sort((a,b) => a.name > b.name ? 1 : -1);
-            setSortOrder(-1);
-        }
-        else{
-            crewList.sort((a,b) => a.name > b.name ? -1 : 1);
-            setSortOrder(1);
-        }
+  const sortData = () => {
+    if (sortOrder === 0 || sortOrder === 1) {
+      crewList.sort((a, b) => (a.name > b.name ? 1 : -1));
+      setSortOrder(-1);
+    } else {
+      crewList.sort((a, b) => (a.name > b.name ? -1 : 1));
+      setSortOrder(1);
     }
+  };
 
-    return(
-        <div className="modalView">
-            <span className="modalTitle">Crew</span>
-            {loading && <div className="spinner"><FontAwesomeIcon icon={faSpinner} spin /></div>}
-            {error && <div>{error}</div>}
-            {!loading && !error &&
-            <>
-                <ul>
-                    <li className="nav">
-                        <span className="dataTitle" onClick={sortData}>
-                            <span >Name</span>
-                            {sortOrder === 0 && <span className="sortStatus arrowsHide">
-                                <FontAwesomeIcon icon={faArrowUp} />
-                                <FontAwesomeIcon icon={faArrowDown} />
-                            </span>} 
-                            {sortOrder === 1 && <span className="sortStatus">
-                                <FontAwesomeIcon icon={faArrowUp} />
-                            </span>}
-                            {sortOrder === -1 && <span className="sortStatus">
-                                <FontAwesomeIcon icon={faArrowDown} />
-                            </span>}
-                        </span>
-                        <span>Status</span>
-                    </li>
-                </ul>
-                <ul className="scroll">
-                    {crewList.map((crew, idx) => <li key={idx}><span>{crew.name}</span><span>{crew.status}</span></li>)}
-                </ul>
-                <div className="liBlur"></div>
-            </>
-            }
+  return (
+    <div className="modalView">
+      <span className="modalTitle">Crew</span>
+      {loading && (
+        <div className="spinner">
+          <FontAwesomeIcon icon={faSpinner} spin />
         </div>
-    )
-}
+      )}
+      {error && <div>{error}</div>}
+      {!loading && !error && (
+        <>
+          <ul>
+            <li className="nav">
+              <span className="dataTitle" onClick={sortData}>
+                <span>Name</span>
+                {sortOrder === 0 && (
+                  <span className="sortStatus arrowsHide">
+                    <FontAwesomeIcon icon={faArrowUp} />
+                    <FontAwesomeIcon icon={faArrowDown} />
+                  </span>
+                )}
+                {sortOrder === 1 && (
+                  <span className="sortStatus">
+                    <FontAwesomeIcon icon={faArrowUp} />
+                  </span>
+                )}
+                {sortOrder === -1 && (
+                  <span className="sortStatus">
+                    <FontAwesomeIcon icon={faArrowDown} />
+                  </span>
+                )}
+              </span>
+              <span>Status</span>
+            </li>
+          </ul>
+          <ul className="scroll">
+            {crewList.map((crew, idx) => (
+              <li key={idx}>
+                <span>{crew.name}</span>
+                <span>{crew.status}</span>
+              </li>
+            ))}
+          </ul>
+          <div className="liBlur"></div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Crew;
